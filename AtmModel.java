7adae0ced815cfb1a8e8de7ad5727529f7968332
katikashivakumar <shivakumar13994@gmail.com>
2025-01-21 21:46:16 +0530

    Create AtmModel.java

---
+++ AtmModel.java
@@ -0,0 +1,8 @@
+public class AtmModel {
+    private int bankBalance = 0;
+    public int getbankBalance() {
+        return bankBalance;}
+    public void setBackBalance(int bankBalance) {
+        this.bankBalance = bankBalance;
+    }
+    }
